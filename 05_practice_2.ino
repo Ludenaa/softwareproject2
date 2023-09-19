ba56a66748e0197080816ffd08df156da30aea0e
이명준 <144985040+Ludenaa@users.noreply.github.com>
2023-09-19 14:15:00 +0900

    Add files via upload

---
+++ 05_practice_2.ino
@@ -0,0 +1,29 @@
+#define PIN_LED 7
+unsigned int toggle;
+
+void setup(){
+  pinMode(PIN_LED, OUTPUT);
+  Serial.begin(115200);
+  while (!Serial){
+    ;
+  }
+  toggle = 0;
+  digitalWrite(PIN_LED, toggle);
+  delay(1000);
+}
+
+void loop(){
+  for(int i = 0; i < 11; i++){
+  toggle = toggle_state(toggle);
+  digitalWrite(PIN_LED, toggle);
+  delay(100);
+  }
+  while(1){}
+}
+
+int toggle_state(int toggle){
+  if(toggle == 0){
+    return 1;
+  }
+  return 0;
+}
